9a38f0ff59ae53f4c2d8a6ee5eb6a8a806c6256c
brocon28 <156925120+brocon28@users.noreply.github.com>
2025-08-05 10:52:15 -0400

    Update allocator.h

--- MemoryAllocatorSimulator/allocator.h
+++ MemoryAllocatorSimulator/allocator.h
@@ -1,1 +1,21 @@
+// allocator.h
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
 
+#include <cstddef>  // for size_t
+
+const size_t HEAP_SIZE = 1024 * 1024; // 1 MB Heap
+
+struct BlockHeader {
+    size_t size;
+    bool is_free;
+    BlockHeader* next;
+};
+
+void initialize_heap();
+void* my_malloc(size_t size);
+void my_free(void* ptr);
+void print_heap_status(); // For debugging
+void print_heap_status();
+
+#endif
